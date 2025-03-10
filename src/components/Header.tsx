ef262d9a2f9f1e37ca9086599bb4837b30585917
Julien Béranger <julien@strat.cc>
2025-03-10 16:03:22 +0100

    Initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,106 @@
+'use client'
+
+import {
+  Box,
+  Button,
+  Flex,
+  Image,
+  Heading,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  IconButton,
+} from '@chakra-ui/react'
+import { useAppKit } from '@reown/appkit/react'
+import { useAppKitAccount, useDisconnect } from '@reown/appkit/react'
+import Link from 'next/link'
+import { HamburgerIcon } from '@chakra-ui/icons'
+import LanguageSelector from './LanguageSelector'
+import { useTranslation } from '@/hooks/useTranslation'
+
+export default function Header() {
+  const { open } = useAppKit()
+  const { isConnected, address } = useAppKitAccount()
+  const { disconnect } = useDisconnect()
+  const t = useTranslation()
+
+  const handleConnect = () => {
+    try {
+      open({ view: 'Connect' })
+    } catch (error) {
+      console.error('Connection error:', error)
+    }
+  }
+
+  const handleDisconnect = () => {
+    try {
+      disconnect()
+    } catch (error) {
+      console.error('Disconnect error:', error)
+    }
+  }
+
+  return (
+    <Box as="header" py={4} position="fixed" w="100%" top={0} zIndex={10}>
+      <Flex justify="space-between" align="center" px={4}>
+        <Link href="/">
+          <Heading as="h3" size="md" textAlign="center">
+            Genji
+          </Heading>
+        </Link>
+        <Flex gap={2} align="center">
+          {!isConnected ? (
+            <Button
+              bg="#8c1c84"
+              color="white"
+              _hover={{
+                bg: '#6d1566',
+              }}
+              onClick={handleConnect}
+              size="sm"
+            >
+              {t.common.login}
+            </Button>
+          ) : (
+            <>
+              <Box transform="scale(0.85)" transformOrigin="right center">
+                {/* <appkit-network-button /> */}
+              </Box>
+              <Button
+                bg="#8c1c84"
+                color="white"
+                _hover={{
+                  bg: '#6d1566',
+                }}
+                onClick={handleDisconnect}
+                size="sm"
+                ml={4}
+              >
+                {t.common.logout}
+              </Button>
+            </>
+          )}
+          <Menu>
+            <MenuButton
+              as={IconButton}
+              aria-label="Options"
+              icon={<HamburgerIcon />}
+              variant="ghost"
+              size="sm"
+            />
+            <MenuList>
+              <Link href="/new" color="white">
+                <MenuItem fontSize="md">{t.navigation.newPage}</MenuItem>
+              </Link>
+              <Link href="/wallet" color="white">
+                <MenuItem fontSize="md">{t.navigation.walletGenerator}</MenuItem>
+              </Link>
+            </MenuList>
+          </Menu>
+          <LanguageSelector />
+        </Flex>
+      </Flex>
+    </Box>
+  )
+}
